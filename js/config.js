1d866666ec239d4179a073b523de0c73b79d04a5
songkiyoun <skycreation1@gmail.com>
2026-05-23 16:47:25 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
 // Apps Script Web App URL을 이곳에 붙여넣습니다.
-// 예: window.CBT_CLOUD_SAVE_API_URL = 'https://script.google.com/macros/s/배포ID/exec';
+// 예: window.CBT_CLOUD_SAVE_API_URL = 'https://script.google.com/macros/s/AKfycbyWV1AKwISvdbzBC8-mP4ze-GAy_z5YIZoxpoi__YXCFsKQULafCs7E4M-urFR1P5Ym8g/exec';
 window.CBT_CLOUD_SAVE_API_URL = '';
